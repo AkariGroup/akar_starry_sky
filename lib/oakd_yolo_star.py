170085a5943298056e301d6dc977e86b3fc6cc46
Kazuya Yamamoto <kazuya_yamamoto@mail.toyota.co.jp>
2024-07-31 22:47:49 +0900

    Update star plotting

--- lib/oakd_yolo_star.py
+++ lib/oakd_yolo_star.py
@@ -1,33 +1,23 @@
 #!/usr/bin/env python
 
-import contextlib
 import copy
 import json
 import math
-import os
 import time
-from dataclasses import dataclass
-from datetime import datetime, timedelta
-from pathlib import Path
-from typing import Any, Dict, List, Optional, Tuple, TypedDict, Union
+from typing import Any, List, Optional, Tuple, Union
 
-import blobconverter
 import cv2
-import depthai as dai
-import matplotlib.pyplot as plt
 import numpy as np
-from mpl_toolkits.mplot3d.art3d import Poly3DCollection
 
-from .util import HostSync, TextHelper
 from akari_yolo_lib.oakd_tracking_yolo import (
     OakdTrackingYolo,
     PosLog,
-    OrbitData,
     OrbitPlayer,
 )
 
 DISPLAY_WINDOW_SIZE_RATE = 2.0
 idColors = np.random.random(size=(256, 3)) * 256
+WHITE = (255, 255, 255)
 
 
 class OakdYoloStar(OakdTrackingYolo):
@@ -76,219 +66,8 @@ class OakdYoloStar(OakdTrackingYolo):
             show_orbit=show_orbit,
             log_path=log_path,
         )
-        self.start_time = datetime.now()
-        self.log_player = LogPlayer()
-
-    def get_frame(self) -> Union[np.ndarray, List[Any], Any]:
-        """フレーム画像と検出結果を取得する。
-
-        Returns:
-            Union[np.ndarray, List[Any]]: フレーム画像と検出結果のリストのタプル。
-
-        """
-        frame = None
-        detections = []
-        ret = False
-        try:
-            ret = self.qRgb.has()
-            if ret:
-                rgb_mes = self.qRgb.get()
-                self.sync.add_msg("rgb", rgb_mes)
-                if self.robot_coordinate:
-                    self.sync.add_msg(
-                        "head_pos",
-                        self.joints.get_joint_positions(),
-                        str(rgb_mes.getSequenceNum()),
-                    )
-        except BaseException:
-            raise
-        ret = False
-        try:
-            ret = self.qDepth.has()
-            if ret:
-                self.sync.add_msg("depth", self.qDepth.get())
-        except BaseException:
-            raise
-        ret = False
-        try:
-            ret = self.qRaw.has()
-            if ret:
-                self.sync.add_msg("raw", self.qRaw.get())
-        except BaseException:
-            raise
-        ret = False
-        try:
-            ret = self.qDet.has()
-            if ret:
-                self.sync.add_msg("detections", self.qDet.get())
-                self.counter += 1
-        except BaseException:
-            raise
-        ret = False
-        try:
-            ret = self.qTrack.has()
-            if ret:
-                self.track = self.qTrack.get()
-        except BaseException:
-            raise
-        msgs = self.sync.get_msgs()
-        tracklets = None
-        if msgs is not None:
-            detections = msgs["detections"].detections
-            frame = msgs["rgb"].getCvFrame()
-            depthFrame = msgs["depth"].getFrame()
-            self.raw_frame = msgs["raw"].getCvFrame()
-            depthFrameColor = cv2.normalize(
-                depthFrame, None, 256, 0, cv2.NORM_INF, cv2.CV_8UC3
-            )
-            depthFrameColor = cv2.equalizeHist(depthFrameColor)
-            depthFrameColor = cv2.applyColorMap(depthFrameColor, self.jet_custom)
-            if self.cam_debug:
-                cv2.imshow("rgb", cv2.resize(frame, (self.width, self.height)))
-                cv2.imshow(
-                    "depth",
-                    cv2.resize(depthFrameColor, (self.width, int(self.width * 3 / 4))),
-                )
-            height = int(frame.shape[1] * 9 / 16)
-            width = frame.shape[1]
-            brank_height = width - height
-            frame = frame[
-                int(brank_height / 2) : int(frame.shape[0] - brank_height / 2),
-                0:width,
-            ]
-            for detection in detections:
-                # Fix ymin and ymax to cropped frame pos
-                detection.ymin = (width / height) * detection.ymin - (
-                    brank_height / 2 / height
-                )
-                detection.ymax = (width / height) * detection.ymax - (
-                    brank_height / 2 / height
-                )
-            if self.track is not None:
-                tracklets = self.track.tracklets
-                for tracklet in tracklets:
-                    # Fix roi to cropped frame pos
-                    tracklet.roi.y = (width / height) * tracklet.roi.y - (
-                        brank_height / 2 / height
-                    )
-                    tracklet.roi.height = tracklet.roi.height * width / height
-
-            if self.robot_coordinate:
-                self.pos = msgs["head_pos"]
-                for detection in detections:
-                    converted_pos = self.convert_to_pos_from_akari(
-                        detection.spatialCoordinates, self.pos["tilt"], self.pos["pan"]
-                    )
-                    detection.spatialCoordinates.x = converted_pos[0][0]
-                    detection.spatialCoordinates.y = converted_pos[1][0]
-                    detection.spatialCoordinates.z = converted_pos[2][0]
-                for tracklet in tracklets:
-                    converted_pos = self.convert_to_pos_from_akari(
-                        tracklet.spatialCoordinates, self.pos["tilt"], self.pos["pan"]
-                    )
-                    tracklet.spatialCoordinates.x = converted_pos[0][0]
-                    tracklet.spatialCoordinates.y = converted_pos[1][0]
-                    tracklet.spatialCoordinates.z = converted_pos[2][0]
-            if self.show_orbit:
-                self.orbit_data_list.update_orbit_data(tracklets)
-        return frame, detections, tracklets
-
-    def get_labeled_frame(
-        self,
-        frame: np.ndarray,
-        tracklets: List[Any],
-        id: Optional[int] = None,
-        disp_info: bool = False,
-    ) -> np.ndarray:
-        """認識結果をフレーム画像に描画する。
-
-        Args:
-            frame (np.ndarray): 画像フレーム。
-            tracklets (List[Any]): トラッキング結果のリスト。
-            id (Optional[int], optional): 描画するオブジェクトのID。指定すると、そのIDのみを描画した画像フレームを返す。指定しない場合は全てのオブジェクトを描画する。
-            disp_info (bool, optional): クラス名とconfidenceをフレーム内に表示するかどうか。デフォルトはFalse。
-
-        Returns:
-            np.ndarray: 描画された画像フレーム。
-
-        """
-        for tracklet in tracklets:
-            if id is not None and tracklet.id != id:
-                continue
-            if tracklet.status.name == "TRACKED":
-                roi = tracklet.roi.denormalize(frame.shape[1], frame.shape[0])
-                x1 = int(roi.topLeft().x)
-                y1 = int(roi.topLeft().y)
-                x2 = int(roi.bottomRight().x)
-                y2 = int(roi.bottomRight().y)
-                try:
-                    label = self.labels[tracklet.label]
-                except Exception:
-                    label = tracklet.label
-                self.text.rectangle(frame, (x1, y1), (x2, y2), idColors[tracklet.id])
-                if disp_info:
-                    self.text.put_text(frame, str(label), (x1 + 10, y1 + 20))
-                    self.text.put_text(
-                        frame,
-                        f"ID: {[tracklet.id]}",
-                        (x1 + 10, y1 + 45),
-                    )
-                    self.text.put_text(frame, tracklet.status.name, (x1 + 10, y1 + 70))
-
-                    if tracklet.spatialCoordinates.z != 0:
-                        self.text.put_text(
-                            frame,
-                            "X: {:.2f} m".format(tracklet.spatialCoordinates.x / 1000),
-                            (x1 + 10, y1 + 95),
-                        )
-                        self.text.put_text(
-                            frame,
-                            "Y: {:.2f} m".format(tracklet.spatialCoordinates.y / 1000),
-                            (x1 + 10, y1 + 120),
-                        )
-                        self.text.put_text(
-                            frame,
-                            "Z: {:.2f} m".format(tracklet.spatialCoordinates.z / 1000),
-                            (x1 + 10, y1 + 145),
-                        )
-        return frame
-
-    def display_frame(self, name: str, frame: np.ndarray, tracklets: List[Any]) -> None:
-        """画像フレームと認識結果を描画する。
-
-        Args:
-            name (str): ウィンドウ名。
-            frame (np.ndarray): 画像フレーム。
-            tracklets (List[Any]): トラッキング結果のリスト。
-            birds(bool): 俯瞰フレームを表示するかどうか。デフォルトはTrue。
-
-        """
-        if frame is not None:
-            frame = cv2.resize(
-                frame,
-                (
-                    int(frame.shape[1] * DISPLAY_WINDOW_SIZE_RATE),
-                    int(frame.shape[0] * DISPLAY_WINDOW_SIZE_RATE),
-                ),
-            )
-            if tracklets is not None:
-                self.get_labeled_frame(frame=frame, tracklets=tracklets, disp_info=True)
-            cv2.putText(
-                frame,
-                "NN fps: {:.2f}".format(
-                    self.counter / (time.monotonic() - self.start_time)
-                ),
-                (2, frame.shape[0] - 4),
-                cv2.FONT_HERSHEY_TRIPLEX,
-                0.3,
-                (255, 255, 255),
-            )
-            # Show the frame
-            cv2.imshow(name, frame)
-            if self.show_bird_frame:
-                self.draw_bird_frame(tracklets)
-            if self.show_spatial_frame:
-                self.draw_spatial_frame(tracklets)
+        self.start_time = time.time()
+        self.log_player = LogPlayer(log_path, start_time=self.start_time)
 
     def create_bird_frame(self) -> np.ndarray:
         """
@@ -299,7 +78,7 @@ class OakdYoloStar(OakdTrackingYolo):
 
         """
         fov = self.fov
-        frame = np.zeros((300, 300, 3), np.uint8)
+        frame = np.zeros((600, 300, 3), np.uint8)
         cv2.rectangle(
             frame, (0, 283), (frame.shape[1], frame.shape[0]), (70, 70, 70), -1
         )
@@ -317,7 +96,7 @@ class OakdYoloStar(OakdTrackingYolo):
                 (0, frame.shape[0]),
             ]
         )
-        cv2.fillPoly(frame, [fov_cnt], color=(70, 70, 70))
+        # cv2.fillPoly(frame, [fov_cnt], color=(70, 70, 70))
         return frame
 
     def update_bird_frame_distance(self, distance: int) -> None:
@@ -360,7 +139,7 @@ class OakdYoloStar(OakdTrackingYolo):
                         (point_x, point_y),
                         2,
                         idColors[tracklets[i].id],
-                        thickness=5,
+                        thickness=3,
                         lineType=8,
                         shift=0,
                     )
@@ -402,7 +181,22 @@ class OakdYoloStar(OakdTrackingYolo):
                                     idColors[tracklets[i].id],
                                     2,
                                 )
-
+                    self.log_player.update_plotting_list(time.time() - self.start_time)
+                    plot_logs = self.log_player.update_plot_data(
+                        time.time() - self.start_time
+                    )
+                    for plot_log in plot_logs:
+                        point_y = self.pos_to_point_y(birds.shape[0], plot_log[1])
+                        point_x = self.pos_to_point_x(birds.shape[1], plot_log[0])
+                        cv2.circle(
+                            birds,
+                            cur_point,
+                            2,
+                            WHITE,
+                            thickness=2,
+                            lineType=8,
+                            shift=0,
+                        )
         cv2.imshow("birds", birds)
 
 
@@ -410,6 +204,7 @@ class LogPlayer(OrbitPlayer):
     def __init__(
         self,
         log_path: str,
+        start_time: int = 0,
         duration: float = 1.0,
         speed: float = 1.0,
         fov: float = 73.0,
@@ -420,7 +215,7 @@ class LogPlayer(OrbitPlayer):
 
         Args:
             log_path (str): ログファイルのパス。
-            duration (float, optional):
+            duration (float, optional): ログのplot追加の時間スケール。デフォルトは1.0。
             speed (float, optional): 再生速度。デフォルトは1.0。
             fov (float, optional): 俯瞰マップ上に描画されるOAK-Dの視野角 (度). デフォルトは 73.0 度。
             max_z (float, optional): 俯瞰マップの最大Z座標値. デフォルトは 15000。
@@ -429,6 +224,11 @@ class LogPlayer(OrbitPlayer):
         self.duration = duration
         self.plotting_list: List[PosLog] = []
         self.plotting_index = 0
+        self.log_path = log_path
+        self.load_log(self.log_path)
+        self.plot_start_time = start_time
+        self.RESTART_INTERVAL = 10 # リセットした際に再度ログをプロットし始めるまでの時間[s]
+        self.last_reset_time = self.plot_start_time
 
     def load_log(self, log_path: str) -> None:
         """
@@ -445,27 +245,49 @@ class LogPlayer(OrbitPlayer):
             print(f"Error: The file {log_path} does not exist.")
             return
 
-    def update_plotting_list(self, cur_time: datetime) -> None:
+    def reset_plotting_log(self, cur_time: int) -> None:
+        """プロットする物体リストをリセットする。"""
+        self.plotting_index = 0
+        self.load_log(self.log_path)
+        self.plot_start_time = cur_time
+        self.last_reset_time = cur_time
+
+    def update_plotting_list(self, cur_time: int) -> None:
+        """
+        指定した時間のプロットする物体リストを更新する。
+
+        Args:
+            cur_time (int): 時間。
+
+        """
         updated_plotting_list = []
         for data in self.plotting_list:
             if self.get_cur_index(cur_time, data) >= 0:
                 updated_plotting_list.append(data)
         while True:
-            if (self.log[self.plotting_index]["time"] - datetime).total_seconds() <= 0:
-                self.plotting_list.append(self.log[self.plotting_index])
+            if self.plotting_index >= len(self.log):
+                self.reset_plotting_log(cur_time)
+                break
+            if (
+                self.log[self.plotting_index]["time"] / self.duration
+                - (cur_time - self.plot_start_time)
+            ) <= 0:
+                # timeを現在時刻に更新した上でplotting_listに追加
+                new_data = copy.deepcopy(self.log[self.plotting_index])
+                new_data["time"] = cur_time
+                self.plotting_list.append(new_data)
+
                 self.plotting_index += 1
             else:
                 break
         self.plotting_list = updated_plotting_list
 
-    def get_plot_pos(
-        self, datetime: datetime, pos_log: PosLog
-    ) -> Optional[Tuple[int, int]]:
+    def get_plot_pos(self, cur_time: int, pos_log: PosLog) -> Optional[Tuple[int, int]]:
         """
         指定した時間の位置を取得する。
 
         Args:
-            datetime (datetime): 時間。
+            cur_time (int): 時間。
             pos_log (PosLog): 時間と位置のログ。
 
         Returns:
@@ -473,7 +295,7 @@ class LogPlayer(OrbitPlayer):
 
         """
         (decimal, index) = math.modf(
-            (datetime - pos_log["time"]) / self.interval * self.speed
+            (cur_time - pos_log["time"]) / self.interval * self.speed
         )
         if index >= len(pos_log["pos"]) - 1:
             return None
@@ -484,12 +306,12 @@ class LogPlayer(OrbitPlayer):
             + pos_log["pos"][index + 1][2] * (1 - decimal),
         )
 
-    def update_plot_data(self, cur_time: datetime) -> List[Tuple[float, float]]:
+    def update_plot_data(self, cur_time: int) -> List[Tuple[float, float]]:
         """
         指定した時間の位置リストを取得する。
 
         Args:
-            cur_time (datetime): 時間。
+            cur_time (int): 時間。
 
         Returns:
             List[Tuple[float, float]]: 位置リスト。
